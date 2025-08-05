6a9ad1de6a34a67b2818d048400627c0d400f8e2
moonbw <ansquddnr4545@gmail.com>
2025-08-05 10:11:07 +0900

    modify: config del

--- city_snap/src/utils/config.js
+++ city_snap/src/utils/config.js
@@ -3,6 +3,6 @@
 // Fast API 연결 주소
 const API_BASE_URL = 'http://192.168.254.107:1234';
 // Google Maps API 키
-const googleMapsApiKey = "AIzaSyDGYOTS7sz5Y9KnDAFy7ENvMO309nw7nng";
+const googleMapsApiKey = "";
 
 export { API_BASE_URL, googleMapsApiKey };
